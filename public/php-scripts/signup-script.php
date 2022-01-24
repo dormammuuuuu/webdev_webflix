bf995b0615e1cdc28ad8431be6c885b19ee19669
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2022-01-25 00:30:05 +0800

    Signup_Script_Update

--- public/php-scripts/signup-script.php
+++ public/php-scripts/signup-script.php
@@ -8,19 +8,51 @@
 
     if(isset($_POST['submit'])){
 
-        if(!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])){
+        if(!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['confirm'])){
 
             $username = $_POST['username'];
             $email = $_POST['email'];
             $password = $_POST['password'];
             $firstName = $_POST['firstname'];
             $lastName = $_POST['lastname'];
+            $confirm = $_POST['confirm'];
     
             $checkUsername = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM `user` WHERE `userName` = '$username'"));
             $checkEmail = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM `user` WHERE `email` = '$email'"));
 
             if($checkUsername == 1 || $checkEmail == 1){
-                echo '
+              if ($checkEmail == 1 && $checkUsername == 1){
+                $message = "Username and Email is already taken";
+                $username = "";
+                $email = "";
+
+              } elseif ($checkUsername == 1 && $checkEmail == 0){
+                $message = "Username is already taken";
+                $username = "";
+              } else {
+                $message = "Email is already taken";
+                $email = "";
+              }
+            
+            }else{
+                if($confirm != $password) {
+                  $message = "Password does not match";
+                } else {
+                    $query = "INSERT INTO `user`(`id`, `firstName`, `lastName`, `userName`, `email`, `avatar`, `password`) VALUES (NULL,'$firstName','$lastName','$username','$email', 'NONE','$password')";
+                    $cmd = mysqli_query($conn,$query) or die(mysqli_error($conn));    
+                    if($cmd){
+                      $firstName = $lastName = $email = $password = $username = "";
+    
+                      header ("location:login.php");
+                    }else{
+                      echo " Registration not Succcesful";
+                    }
+                }
+            }
+        }else{
+            $message = "All fields required";
+        }
+        echo '
                 <div id="toast-id">
                   <div class="toast">
                     <div class="toast-container">
@@ -28,7 +60,7 @@
                         <div class="toast-icon">
                           <i class="fas fa-exclamation"></i>
                         </div>
-                        <p class="toast-message"></p>
+                        <p class="toast-message">'.$message.'</p>
                       </div>
                       <div class="toast-dismiss">
                         <i class="fas fa-times"></i>
@@ -38,55 +70,5 @@
                   </div>
                 </div>
                 ';
-
-              if ($checkEmail == 1 && $checkUsername == 1){
-                echo '
-                <script>
-                  $(function () {
-                    $(".toast-message").text("Username and Email is already taken.");
-                  });
-                </script>
-                
-                ';
-                $username = "";
-                $email = "";
-
-              } elseif ($checkUsername == 1 && $checkEmail == 0){
-                echo '
-                <script>
-                  $(function () {
-                    $(".toast-message").text("Username is already taken");
-                  });
-                </script>
-               
-                ';
-                $username = "";
-              } else {
-                echo '
-                <script>
-                  $(function () {
-                    $(".toast-message").text("Email is already taken");
-                  });
-                </script>
-              
-                ';
-                $email = "";
-              }
-            
-            }else{
-                
-                $query = "INSERT INTO `user`(`id`, `firstName`, `lastName`, `userName`, `email`, `avatar`, `password`) VALUES (NULL,'$firstName','$lastName','$username','$email', 'NONE','$password')";
-                $cmd = mysqli_query($conn,$query) or die(mysqli_error($conn));    
-                if($cmd){
-                   $firstName = $lastName = $email = $password = $username = "";
-
-                   header ("location:login.php");
-                }else{
-                   echo " Registration not Succcesful";
-                }
-            }
-        }else{
-            echo "all fields required!";
-        }
     }
 ?>
