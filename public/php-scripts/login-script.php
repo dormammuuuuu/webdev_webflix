cf73116a5a29df8550e5cfea82c7673dab083742
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2022-02-02 00:39:59 +0800

    xx

--- public/php-scripts/login-script.php
+++ public/php-scripts/login-script.php
@@ -5,47 +5,46 @@
     if(ISSET($_SESSION['id'])){
       header("location:home.php");
     }
-    $message;
-    if(isset($_POST['submit'])){
-        if(!empty($_POST['email']) && !empty($_POST['password'])){
+      if(isset($_POST['submit'])){
+      if(!empty($_POST['email']) && !empty($_POST['password'])){
 
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $query = "SELECT * FROM user WHERE email = '$email' AND password = '$password'";
+          $email = $_POST['email'];
+          $password = $_POST['password'];
+          $query = "SELECT * FROM user WHERE email = '$email' AND password = '$password'";
 
-            $cmd = mysqli_query($conn,$query);
-            $row = mysqli_fetch_array($cmd);
-            $count = mysqli_num_rows($cmd);
+          $cmd = mysqli_query($conn,$query);
+          $row = mysqli_fetch_array($cmd);
+          $count = mysqli_num_rows($cmd);
 
-            if($count == 1){
-                session_start();
-                $_SESSION['id'] = $row['id'];
-                header("location:home.php");
-            }else{
-              $message = "The username or password you've entered is incorrect.";
-               
-            }
+          if($count == 1){
+              session_start();
+              $_SESSION['id'] = $row['id'];
+              header("location:home.php");
+          }else{
+            $message = "The username or password you've entered is incorrect.";
+            
+          }
 
-        }else{
-            $message = "All fields required.";
-        }
-        echo '
-          <div id="toast-id">
-            <div class="toast">
-              <div class="toast-container">
-                <div class="toast-content">
-                  <div class="toast-icon">
-                    <i class="fas fa-exclamation"></i>
-                  </div>
-                  <p class="toast-message">'.$message.'</p>
+      }else{
+          $message = "All fields required.";
+      }
+      echo '
+        <div id="toast-id">
+          <div class="toast">
+            <div class="toast-container">
+              <div class="toast-content">
+                <div class="toast-icon">
+                  <i class="fas fa-exclamation"></i>
                 </div>
-                <div class="toast-dismiss">
-                  <i class="fas fa-times"></i>
-                </div>
+                <p class="toast-message">'.$message.'</p>
               </div>
-              <div id="toast-progress"></div>
+              <div class="toast-dismiss">
+                <i class="fas fa-times"></i>
+              </div>
             </div>
+            <div id="toast-progress"></div>
           </div>
-          ';
+        </div>
+        ';
     }
 ?>
